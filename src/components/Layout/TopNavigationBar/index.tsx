1ba3b7b5603230b6daf3cb03b078db4b07ae1e35
chichoon <chichoon.choi@gmail.com>
2022-05-31 01:00:07 +0900

    Design: 상단바 아이콘 스타일링

--- src/components/Layout/TopNavigationBar/index.tsx
+++ src/components/Layout/TopNavigationBar/index.tsx
@@ -1,12 +1,18 @@
+import { FeatherIcon } from 'assets/svgs';
 import styles from './topNavigationBar.module.scss';
 
 const TopNavigationBar = () => {
   return (
     <nav className={styles.navigationBarWrapper}>
-      <h3>
-        <b>chichoon</b>님 안녕하세요!
-      </h3>
-      {/* TODO: 닉네임 교체 */}
+      <div className={styles.navigationBarIcon}>
+        <FeatherIcon />
+      </div>
+      <div className={styles.navigationBarRight}>
+        <h3>
+          <b>chichoon</b>님 안녕하세요!
+        </h3>
+        {/* TODO: 닉네임 교체 */}
+      </div>
     </nav>
   );
 };
