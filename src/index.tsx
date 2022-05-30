40de94bacae12ea6fffa3f7a5151de953f8df46f
chichoon <chichoon.choi@gmail.com>
2022-05-30 17:07:52 +0900

    Feat: js to ts

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import Router from 'router';
+
+import reportWebVitals from './reportWebVitals';
+
+import 'styles/index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
