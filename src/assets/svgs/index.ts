720b7fed9be96609c1f4e9347f9acd99c68757e0
chichoon <chichoon.choi@gmail.com>
2022-05-30 18:18:37 +0900

    Feat: 아이콘 추가

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as DashboardIcon } from './dashboardIcon.svg';
+export { ReactComponent as SettingsIcon } from './settingsIcon.svg';
+export { ReactComponent as StatsIcon } from './statisticsIcon.svg';
