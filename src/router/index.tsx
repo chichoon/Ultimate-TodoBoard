8e7eba08cdc5de640edf9d744e6097a5ee470681
chichoon <chichoon.choi@gmail.com>
2022-05-30 19:09:11 +0900

    Feat: 루트 경로에서 dashboard로 리디렉션 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
 import { Layout } from 'components';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Dashboard from './Dashboard';
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<Navigate replace to='dashboard' />} />
+        <Route path='dashboard' element={<Dashboard />} />
       </Route>
     </Routes>
   );
