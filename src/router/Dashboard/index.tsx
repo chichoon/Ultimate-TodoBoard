3fa1689ae8003e457f0fcec41e0ba2f7c707a5ac
chichoon <chichoon.choi@gmail.com>
2022-05-30 17:57:24 +0900

    Feat: 레이아웃에 대시보드 심기

--- src/router/Dashboard/index.tsx
+++ src/router/Dashboard/index.tsx
@@ -11,4 +11,11 @@ const Dashboard = () => {
   );
 };
 
+// TODO: 백준 컨테이너
+// TODO: 오늘의 할일 컨테이너
+// TODO: 디데이 컨테이너
+// TODO: 날씨 컨테이너
+// TODO: 깃허브 잔디 컨테이너
+// TODO: 임베드 컨테이너
+
 export default Dashboard;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,13 @@
+import { Layout } from 'components';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<Dashboard />} />
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 };
