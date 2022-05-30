312fd381f02fafa76adf47d92e57d221667ede4f
chichoon <chichoon.choi@gmail.com>
2022-05-30 18:07:36 +0900

    Feat: navBar 추가

---
+++ src/components/Layout/NavigationBar/index.tsx
@@ -0,0 +1,11 @@
+import styles from './navigationBar.module.scss';
+
+const NavigationBar = () => {
+  return (
+    <nav className={styles.navigationBarWrapper}>
+      <ul />
+    </nav>
+  );
+};
+
+export default NavigationBar;
