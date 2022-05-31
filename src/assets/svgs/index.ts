6e2096948b01e102a0ca0c4b87ed3fe4572fe163
chichoon <chichoon.choi@gmail.com>
2022-05-31 18:17:34 +0900

    Feat: X 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as SettingsIcon } from './settingsIcon.svg';
 export { ReactComponent as StatsIcon } from './statisticsIcon.svg';
 export { ReactComponent as FeatherIcon } from './featherIcon.svg';
 export { ReactComponent as HamburgerIcon } from './hamburgerIcon.svg';
+export { ReactComponent as XIcon } from './xIcon.svg';
