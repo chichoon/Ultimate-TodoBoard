b76ed55eabf4ce5cd90b3b63df6ef18c9fa63284
chichoon <chichoon.choi@gmail.com>
2022-05-31 13:29:40 +0900

    Feat: 햄버거 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,3 +2,4 @@ export { ReactComponent as DashboardIcon } from './dashboardIcon.svg';
 export { ReactComponent as SettingsIcon } from './settingsIcon.svg';
 export { ReactComponent as StatsIcon } from './statisticsIcon.svg';
 export { ReactComponent as FeatherIcon } from './featherIcon.svg';
+export { ReactComponent as HamburgerIcon } from './hamburgerIcon.svg';
