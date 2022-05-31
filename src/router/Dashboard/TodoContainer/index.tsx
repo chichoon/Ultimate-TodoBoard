56bd8d4590abc3a38d91e26c3e697b5cace53d79
chichoon <chichoon.choi@gmail.com>
2022-05-31 15:07:26 +0900

    Design: flex-grow 속성으로 남은 공간 딱 맞게 설정

--- src/router/Dashboard/TodoContainer/index.tsx
+++ src/router/Dashboard/TodoContainer/index.tsx
@@ -1,11 +1,13 @@
 import { ITodo } from 'types/todo';
 
+import styles from './todoContainer.module.scss';
+
 interface IProps {
   item: ITodo;
 }
 
 const TodoContainer = ({ item }: IProps) => {
-  return <div>투두 컨테이너지롱</div>;
+  return <div className={styles.todoWrapper}>투두 컨테이너지롱</div>;
 };
 
 export default TodoContainer;
