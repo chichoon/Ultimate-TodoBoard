47e0519e797cb6cee8cc5b940407ac0cf9cfe259
chichoon <chichoon.choi@gmail.com>
2022-06-01 00:06:06 +0900

    Feat: 라우터 마운트 시에 오늘의 날짜 세팅

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,21 @@
-import { Layout } from 'components';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useMount } from 'react-use';
+import { useSetRecoilState } from 'recoil';
+import dayjs from 'dayjs';
 
+import { Layout } from 'components';
+import { todayDateState } from 'states/atoms';
 import Dashboard from './Dashboard';
 import Settings from './Settings';
 import Statistics from './Statistics';
 
 const Router = () => {
+  const setTodayDate = useSetRecoilState(todayDateState);
+
+  useMount(() => {
+    setTodayDate(dayjs().format('YYYY-MM-DD'));
+  });
+
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
