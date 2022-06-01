257041691659fd941f246a0c608f874a1e4852cb
chichoon <chichoon.choi@gmail.com>
2022-06-01 18:33:23 +0900

    Feat: 백준 리스트 컴포넌트 레이아웃 완료

--- src/router/Dashboard/BaekjoonContainer/BaekjoonListElement/index.tsx
+++ src/router/Dashboard/BaekjoonContainer/BaekjoonListElement/index.tsx
@@ -1,17 +1,36 @@
+import cx from 'classnames';
+
 import { IBaekjoonItem } from 'types/information';
-import solvedacIcon from './SolvedacIcon';
+import solvedacIcon from './solvedacIcon';
 
 import styles from './baekjoonListElement.module.scss';
 
 interface IProps {
   item: IBaekjoonItem;
+  index: number;
 }
 
-const BaekjoonListElement = ({ item }: IProps) => {
+const BaekjoonListElement = ({ item, index }: IProps) => {
+  const tierClass = `tier${Math.floor((item.tier + 4) / 5)}`;
+
+  const handleDoneButtoClick = () => {
+    console.log(`삭제 ${index}`);
+  };
+
   return (
-    <div className={styles.baekjoonElementWrapper}>
-      {solvedacIcon(item.tier)}
-      <p className={styles.baekjoonTitle}>{item.problem}</p>
+    <div className={cx(styles.baekjoonElementWrapper, styles[tierClass])}>
+      <a href={`https://www.acmicpc.net/problem/${item.id}`} target='_blank' rel='noreferrer'>
+        {solvedacIcon(item.tier)}
+        <dl className={styles.baekjoonInformation}>
+          <dt>{item.id}</dt>
+          <dd>{item.problem}</dd>
+        </dl>
+      </a>
+      <button type='button' className={styles.baekjoonDoneButton} onClick={handleDoneButtoClick}>
+        완료
+      </button>
     </div>
   );
 };
+
+export default BaekjoonListElement;
