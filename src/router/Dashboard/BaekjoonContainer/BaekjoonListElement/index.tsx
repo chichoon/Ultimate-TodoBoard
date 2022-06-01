95eceb9a369f85dc980c98fe6d6c696e230aa92f
chichoon <chichoon.choi@gmail.com>
2022-06-02 00:09:13 +0900

    Feat: div 안에 div 감싸는거 전부 제거

--- src/router/Dashboard/BaekjoonContainer/BaekjoonListElement/index.tsx
+++ src/router/Dashboard/BaekjoonContainer/BaekjoonListElement/index.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import cx from 'classnames';
 
 import { IBaekjoonItem } from 'types/information';
 import solvedacIcon from './solvedacIcon';
+import { DeleteConfirmModal } from 'router/Dashboard/_shared';
 
 import styles from './baekjoonListElement.module.scss';
+import { XIcon } from 'assets/svgs';
 
 interface IProps {
   item: IBaekjoonItem;
@@ -12,23 +15,46 @@ interface IProps {
 
 const BaekjoonListElement = ({ item, index }: IProps) => {
   const tierClass = `tier${Math.floor((item.tier + 4) / 5)}`;
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
-  const handleDoneButtoClick = () => {
+  const handleXButtonClick = () => {
     console.log(`삭제 ${index}`);
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleDeleteButtonClick = () => {
+    console.log('삭제');
+    setIsDeleteModalOpen(false);
+  };
+
+  const handleCancelButtonClick = () => {
+    setIsDeleteModalOpen(false);
   };
 
   return (
     <div className={cx(styles.baekjoonElementWrapper, styles[tierClass])}>
       <a href={`https://www.acmicpc.net/problem/${item.id}`} target='_blank' rel='noreferrer'>
-        {solvedacIcon(item.tier)}
-        <dl className={styles.baekjoonInformation}>
-          <dt>{item.id}</dt>
-          <dd>{item.problem}</dd>
-        </dl>
+        <div className={styles.baekjoonElementLeft}>
+          {solvedacIcon(item.tier)}
+          <p className={styles.baekjoonID}>{item.id}</p>
+          <p className={styles.baekjoonProblem}>{item.problem}</p>
+        </div>
       </a>
-      <button type='button' className={styles.baekjoonDoneButton} onClick={handleDoneButtoClick}>
-        완료
+      <button type='button' className={styles.baekjoonXButton} onClick={handleXButtonClick}>
+        <XIcon />
       </button>
+      {isDeleteModalOpen && (
+        <DeleteConfirmModal title={item.problem}>
+          <>
+            <button type='button' onClick={handleDeleteButtonClick}>
+              확인
+            </button>
+            <button type='button' onClick={handleCancelButtonClick}>
+              취소
+            </button>
+          </>
+        </DeleteConfirmModal>
+      )}
     </div>
   );
 };
