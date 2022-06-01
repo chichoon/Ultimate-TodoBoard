d28c7f26af6cb309c626f3bac1008b68cce2ac27
chichoon <chichoon.choi@gmail.com>
2022-06-01 21:50:57 +0900

    Fix: overflow scroll 안되던 문제 해결

--- src/router/Dashboard/index.tsx
+++ src/router/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import DashboardItem from './DashboardItem';
 const Dashboard = () => {
   return (
     <main className={styles.dashboardMain}>
-      <ul>
+      <ul className={styles.dashboardListWrapper}>
         <DashboardItem type='todo' />
         <DashboardItem type='baekjoon' />
         <li /> {/* TODO: 대시보드 아이템 추가하는 li 만들기 */}
@@ -12,13 +12,5 @@ const Dashboard = () => {
     </main>
   );
 };
-
-// TODO: 오늘의 할일 컨테이너
-// TODO: 디데이 컨테이너
-// TODO: 날씨 컨테이너
-// TODO: 백준 컨테이너
-// TODO: 깃허브 잔디 컨테이너
-// TODO: 이미지 링크 컨테이너
-// TODO: 임베드 컨테이너
 
 export default Dashboard;
