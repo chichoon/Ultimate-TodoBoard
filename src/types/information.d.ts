8fc5e98e25b666d4ae791f06364d1a70fbaaecb2
chichoon <chichoon.choi@gmail.com>
2022-06-01 14:54:22 +0900

    Feat: 사용할 타입 정리

---
+++ src/types/information.d.ts
@@ -0,0 +1,24 @@
+export interface IInformation {
+  baekjoon: IBaekjoonItem[];
+  weather: IWeather;
+  dday: IDDay[];
+  githubID: string;
+}
+
+export interface IBaekjoonItem {
+  id: number;
+  problem: string;
+  tier: number;
+}
+
+export interface IWeather {
+  locationName: string;
+  lat: number;
+  lon: number;
+}
+
+export interface IDDay {
+  title: string;
+  icon: string;
+  date: string;
+}
