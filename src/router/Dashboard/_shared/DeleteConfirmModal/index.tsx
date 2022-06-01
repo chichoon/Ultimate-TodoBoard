25864e661b972e11f90a3790bc58ce91cb51500e
chichoon <chichoon.choi@gmail.com>
2022-06-02 00:21:30 +0900

    Feat: 불필요한 Flexbox 제거

--- src/router/Dashboard/_shared/DeleteConfirmModal/index.tsx
+++ src/router/Dashboard/_shared/DeleteConfirmModal/index.tsx
@@ -1,3 +1,4 @@
+import ModalPortal from '../modalPortal';
 import styles from './deleteConfirmModal.module.scss';
 
 interface IProps {
@@ -7,17 +8,19 @@ interface IProps {
 
 const DeleteConfirmModal = ({ title, children }: IProps) => {
   return (
-    <div className={styles.modalBackground}>
-      <div className={styles.deleteModalWrapper}>
-        <header>
-          <p>정말로 삭제하시겠습니까?</p>
-        </header>
-        <main>
-          <p>{title}</p>
-          <div className={styles.buttonWrapper}>{children}</div>
-        </main>
+    <ModalPortal>
+      <div className={styles.modalBackground}>
+        <div className={styles.deleteModalWrapper}>
+          <header>
+            <p>정말로 삭제하시겠습니까?</p>
+          </header>
+          <main>
+            <p>{title}</p>
+            <div className={styles.buttonWrapper}>{children}</div>
+          </main>
+        </div>
       </div>
-    </div>
+    </ModalPortal>
   );
 };
 
