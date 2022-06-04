5f7b2b46cdcbb0f3dcf21b9eb3a3e99f4fca8591
chichoon <chichoon.choi@gmail.com>
2022-06-05 07:37:00 +0900

    Design: 바로가기

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLocation } from 'react-use';
 import { Outlet } from 'react-router-dom';
 import ErrorMessage from './ErrorMessage';
 
@@ -8,6 +9,14 @@ import TopNavigationBar from './TopNavigationBar';
 
 const Layout = () => {
   const [isBarHidden, setIsBarHidden] = useState(true);
+  const location = useLocation();
+  const { pathname } = location;
+  const titleString = {
+    '/': '대시보드',
+    '/dashboard': '대시보드',
+    '/settings': '설정',
+    '/statistics': '통계',
+  }[pathname ?? '/'];
 
   return (
     <div className={styles.layoutWrapper}>
@@ -16,6 +25,9 @@ const Layout = () => {
       <div className={styles.layoutBottom}>
         <SideNavigationBar isBarHidden={isBarHidden} />
         <div className={styles.layoutOutlet}>
+          <header className={styles.dashboardHeader}>
+            <h1>{titleString}</h1>
+          </header>
           <Outlet />
         </div>
       </div>
