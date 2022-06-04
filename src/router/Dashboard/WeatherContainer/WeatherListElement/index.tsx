d5008a1dd1a0b2d7e6c37952cb1b818f6b10c53e
chichoon <chichoon.choi@gmail.com>
2022-06-05 04:25:02 +0900

    Design: 날씨 리스트 컨테이너 디자인 갈아엎기

--- src/router/Dashboard/WeatherContainer/WeatherListElement/index.tsx
+++ src/router/Dashboard/WeatherContainer/WeatherListElement/index.tsx
@@ -21,21 +21,22 @@ const WeatherListElement = ({ item }: IProps) => {
 
   return (
     <li className={cx(styles.weatherElementWrapper, weatherClassname)}>
-      <img
-        src={`https://openweathermap.org/img/wn/${item.icon}@2x.png`}
-        alt={`${item.icon}.png`}
-        onError={handleImgError}
-      />
       <div className={styles.weatherElementTop}>
-        <p>{dayjsTime.format('YYYY년 MM월 DD일 HH시')}</p>
-        <p className={styles.weatherElementImportant}>
-          {item.temp}ºC {item.weather}
-        </p>
+        <img
+          src={`https://openweathermap.org/img/wn/${item.icon}@2x.png`}
+          alt={`${item.icon}.png`}
+          onError={handleImgError}
+        />
+        <div className={styles.weatherElementInfo}>
+          <p>{dayjsTime.format('YYYY년 MM월 DD일 HH시')}</p>
+          <p className={styles.weatherElementImportant}>
+            {item.temp}ºC {item.weather}
+          </p>
+        </div>
       </div>
       <div className={styles.weatherElementBottom}>
-        <p className={styles.weatherTemperature}>
-          최저 {item.temp_min}ºC 최고 {item.temp_max}ºC
-        </p>
+        <p className={styles.weatherTemperature}>최저 {item.temp_min}ºC</p>
+        <p className={styles.weatherTemperature}>최고 {item.temp_max}ºC</p>
         <p>습도 {item.humidity}%</p>
       </div>
     </li>
