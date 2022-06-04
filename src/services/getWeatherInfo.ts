7de8e1decadc02369ceae390e9fa22c3d7de9ba5
chichoon <chichoon.choi@gmail.com>
2022-06-04 13:11:01 +0900

    Feat: 설정한 타입으로 getWeatherInfo 재정의

--- src/services/getWeatherInfo.ts
+++ src/services/getWeatherInfo.ts
@@ -1,8 +1,10 @@
+import { IWeatherData } from 'types/weather.d';
+import { IList, IWeatherResponse } from 'types/weatherResponse.d';
 import axios from 'axios';
 
 const getWeatherInfo = (lat: number, lon: number) =>
   axios
-    .get('https://api.openweathermap.org/data/2.5/weather', {
+    .get('https://api.openweathermap.org/data/2.5/forecast', {
       params: {
         lat,
         lon,
@@ -10,8 +12,22 @@ const getWeatherInfo = (lat: number, lon: number) =>
         units: 'metric',
       },
     })
-    .then((response) => {
-      console.log(response);
+    .then(({ data }: { data: IWeatherResponse }) => {
+      const newArr: IWeatherData = {
+        city: data.city.name ?? '선택한 도시',
+        weatherList: data.list.map((item: IList) => {
+          return {
+            time: item.dt_txt,
+            temp: item.main.temp,
+            temp_min: item.main.temp_min,
+            temp_max: item.main.temp_max,
+            humidity: item.main.humidity,
+            weather: item.weather[0].main,
+          };
+        }),
+      };
+      console.log(newArr);
+      return newArr;
     });
 
 export default getWeatherInfo;
