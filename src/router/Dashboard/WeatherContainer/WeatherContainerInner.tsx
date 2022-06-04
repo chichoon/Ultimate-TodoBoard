0130a8330634e45ec7085ca6a6ca182e81714a9f
chichoon <chichoon.choi@gmail.com>
2022-06-04 17:18:34 +0900

    Design: 날씨 컨테이너 디자인 완료

--- src/router/Dashboard/WeatherContainer/WeatherContainerInner.tsx
+++ src/router/Dashboard/WeatherContainer/WeatherContainerInner.tsx
@@ -13,7 +13,7 @@ const WeatherContainerInner = () => {
       <div className={cx('listContainerHeader')}>
         <h3>{data?.city ?? '선택한 도시'}의 날씨</h3>
       </div>
-      <div className={styles.weatherItemsWrapper}>
+      <div className={styles.weatherListWrapper}>
         <ul>
           {data?.weatherList.map((item) => {
             const key = `weather-${item.time}`;
