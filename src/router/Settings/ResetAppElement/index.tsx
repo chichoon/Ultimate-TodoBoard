bcee40916df1f90bf338bcd2940572e172113faa
chichoon <chichoon.choi@gmail.com>
2022-06-05 07:42:02 +0900

    Design: 세팅 페이지 grid 적용

--- src/router/Settings/ResetAppElement/index.tsx
+++ src/router/Settings/ResetAppElement/index.tsx
@@ -4,7 +4,7 @@ import styles from './resetAppElement.module.scss';
 
 const ResetAppElement = () => {
   return (
-    <div className={cx(styles.settingsFormWrapper, 'listContainer')}>
+    <div className={cx(styles.resetFormWrapper, 'listContainer')}>
       <div className={cx('listContainerHeader')}>
         <h3>데이터 리셋</h3>
       </div>
