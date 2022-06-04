332f997652c6649c6b5e9f504100d5c11a851588
chichoon <chichoon.choi@gmail.com>
2022-06-04 21:45:55 +0900

    Feat: error Icon 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -42,3 +42,4 @@ export { ReactComponent as PlusIcon } from './plusIcon.svg';
 export { ReactComponent as ColorFillIcon } from './colorFillIcon.svg';
 export { ReactComponent as DownArrowIcon } from './downArrowIcon.svg';
 export { ReactComponent as LoadingIcon } from './loadingIcon.svg';
+export { ReactComponent as ErrorIcon } from './errorIcon.svg';
