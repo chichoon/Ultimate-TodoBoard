f77b4800153cfe916804fe2a4823335a75b1e699
chichoon <chichoon.choi@gmail.com>
2022-06-11 16:19:49 +0900

    Feat: Solved.ac 컴포넌트 추가

--- src/router/Statistics/SolvedacContainer/index.tsx
+++ src/router/Statistics/SolvedacContainer/index.tsx
@@ -1,12 +1,16 @@
 import cx from 'classnames';
 
+import { useAppSelector } from 'hooks';
+import { getSolvedacID } from 'states/settings';
+
 import styles from './solvedacContainer.module.scss';
 
 const SolvedacContainer = () => {
+  const solvedacID = useAppSelector(getSolvedacID);
   return (
     <li className={cx(styles.solvedacWrapper, 'listContainer')}>
       <div className={cx('listContainerHeader')}>
-        <h3>baekjoonID의 Solved.ac 현황</h3>
+        <h3>{solvedacID}의 Solved.ac 현황</h3>
       </div>
     </li>
   );
