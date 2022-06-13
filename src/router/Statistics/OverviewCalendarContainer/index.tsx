ad647d5e17c1e0aab2bd9c100730f6b20b994bb9
chichoon <chichoon.choi@gmail.com>
2022-06-13 21:08:20 +0900

    Fix: 안 쓰는 라인 일단 삭제

--- src/router/Statistics/OverviewCalendarContainer/index.tsx
+++ src/router/Statistics/OverviewCalendarContainer/index.tsx
@@ -1,17 +1,10 @@
 import Calendar from 'react-calendar';
 import cx from 'classnames';
 
-import { useAppSelector } from 'hooks';
-import { getTodo } from 'states/todo';
-import { getDDays } from 'states/information';
-
 import 'react-calendar/dist/Calendar.css';
 import styles from './overviewCalendarContainer.module.scss';
 
 const OverviewCalendarContainer = () => {
-  const todo = useAppSelector(getTodo);
-  const dday = useAppSelector(getDDays);
-
   return (
     <li className={cx(styles.calendarWrapper, 'listContainer')}>
       <div className={cx('listContainerHeader')}>
